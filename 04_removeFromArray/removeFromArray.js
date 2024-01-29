1ae840af6ac159437800d00105cd155f4c39ddf9
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-01-29 11:52:39 +0100

    exercise not completed / study arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(a,...b) {
+    const checkArray = [];
+    a.forEach((item) => {
+        if (!b.includes(item)) {
+            checkArray.push(item);
+          });
+    return checkArray;
 
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
