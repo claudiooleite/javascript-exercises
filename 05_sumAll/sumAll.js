f23d5ae52e13e25436492bdc69f716f926233f1c
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-01-29 16:54:14 +0100

    exercise reviewed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum,secondNum) {
+    let sum = 0;
 
-};
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return `ERROR`;
+    
+    if (firstNum < 0 || secondNum < 0) return `ERROR`; 
+    
+    if (firstNum > secondNum) [firstNum, secondNum] = [secondNum, firstNum];
+     
+    for (i = firstNum;i<=secondNum; i++) {
+           sum += i;
+        }
+     return sum;
+    }
+    
 
 // Do not edit below this line
 module.exports = sumAll;
